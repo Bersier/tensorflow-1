ae2bb3516a5b4288cceabfd7d52699a35bd461d4
Stephane Bersier <stephane.bersier@gmail.com>
2020-08-10 17:19:52 -0700

    Looks like code builds

--- src/type/tensor/structured/type/make.py
+++ src/type/tensor/structured/type/make.py
@@ -17,19 +17,19 @@ NUMERIC_DTYPES = {
 }
 
 
-# noinspection PyArgumentList
-def sum2(first_or_second: Tuple[Name, T1], second_or_first: Tuple[Name, T2]):
-    if isinstance(first_or_second[1], Numeric) and isinstance(second_or_first[1], Numeric):
-        return numeric.Sum2(first_or_second, second_or_first)
-    if isinstance(first_or_second[1], Boolean) and isinstance(second_or_first[1], Boolean):
-        return boolean.Sum2(first_or_second, second_or_first)
-    return core.Sum2(first_or_second, second_or_first)
-
-
-# noinspection PyArgumentList
-def primitive(of: tf.dtypes.DType) -> Primitive:
-    if of in NUMERIC_DTYPES:
-        return numeric.Primitive(of)
-    if of == tf.dtypes.bool:
-        return boolean.Primitive(of)
-    return core.Primitive(of)
+# # noinspection PyArgumentList
+# def sum2(first_or_second: Tuple[Name, T1], second_or_first: Tuple[Name, T2]):
+#     if isinstance(first_or_second[1], Numeric) and isinstance(second_or_first[1], Numeric):
+#         return numeric.Sum2(first_or_second, second_or_first)
+#     if isinstance(first_or_second[1], Boolean) and isinstance(second_or_first[1], Boolean):
+#         return boolean.Sum2(first_or_second, second_or_first)
+#     return core.Sum2(first_or_second, second_or_first)
+#
+#
+# # noinspection PyArgumentList
+# def primitive(of: tf.dtypes.DType) -> Primitive:
+#     if of in NUMERIC_DTYPES:
+#         return numeric.Primitive(of)
+#     if of == tf.dtypes.bool:
+#         return boolean.Primitive(of)
+#     return core.Primitive(of)
